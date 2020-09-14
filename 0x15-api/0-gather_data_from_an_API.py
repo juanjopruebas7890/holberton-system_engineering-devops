b805c2d828e913a1839c58aab99ff5eedebdf7da
Juan Jose <1561@holbertonschool.com>
2020-09-14 23:52:59 +0000

    [add]2

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,13 +5,15 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    
     url_todo = 'https://jsonplaceholder.typicode.com/todos/'
+    
     url_user = 'https://jsonplaceholder.typicode.com/users/'
+    
     emp_id = argv[1]
     todo = requests.get(url_todo, params={'userID': emp_id})
     user = requests.get(url_user, params={'id': emp_id})
     todo_list = todo.json()
+    
     user_list = user.json()
     tasks = []
     all_tasks = len(todo_list)
